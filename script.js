de9da0f5c2b3c153f62c917324e7d09960d1f0a7
Nicolaau <nicholasrieger2@gmail.com>
2025-12-18 12:09:31 -0300

    Criação do validador simples de e-mail
    Utilização de .includes para verificar se o caracter @ e .com estão inclusos na string.
    
    Chamada do validador quando o botão "Validar" for clicado, e limpeza do campo caso esteja ok para uma resposta visual de que o e-mail foi enviado.

---
+++ script.js
@@ -0,0 +1,22 @@
+const inputEmail = document.querySelector('#email')
+const botaoValidar = document.querySelector('.botaoValidar')
+const resultado = document.querySelector('.resultado')
+
+function validacao(valor){
+    const email = valor.trim()
+    return email.includes('@') && email.includes('.com')
+}
+
+botaoValidar.addEventListener('click', () => {
+    const ok = validacao(inputEmail.value)
+
+    if(ok){
+        resultado.textContent = 'E-mail validado'
+        resultado.style.color = 'green'
+        inputEmail.value = ''
+    } else {
+        resultado.textContent = 'E-mail é inválido'
+        resultado.style.color = 'red'
+    }
+})
+
